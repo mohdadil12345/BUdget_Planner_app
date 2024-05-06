bfa9891413ced71b987b3952c627eeac1e477c23
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-05-06 19:52:15 +0530

    signup form

--- budget-planner/src/app/budget-planner/login/login.component.ts
+++ budget-planner/src/app/budget-planner/login/login.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+
+  signupdata : FormGroup
+
+  constructor (private formBuilder: FormBuilder) {
+      this.signupdata = this.formBuilder.group({
+        username : ["",  Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+
+      })
+  }
+
+
+
+  signup_form = () => {
+      console.log(this.signupdata.value)  
+    
+  }
+
 
 }
