4fd4fbe97231d679395565a67ba0413dffa67d48
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-05-07 22:20:33 +0530

    income ts

---
+++ budget-planner/src/app/budget-planner/income/income.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-income',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './income.component.html',
+  styleUrl: './income.component.scss'
+})
+export class IncomeComponent {
+
+  incomeForm : any
+
+  constructor(public fb : FormBuilder) {}
+
+  ngOnInit() : void {
+    this.incomeForm = this.fb.group({
+      month : ["",  Validators.required],
+      source: ['', Validators.required],
+      amount: ['', Validators.required],
+      investments : ['', Validators.required],
+  })
+
+}
+
+
+
+}
